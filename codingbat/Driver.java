27bd533926d6503046d83e7340f957aa4d45c1a3
david.kerrigan <david19kerrigan@gmail.com>
2014-09-24 09:14:21 -0400

    D

--- codingbat/Driver.java
+++ codingbat/Driver.java
@@ -1,4 +1,20 @@
-
+public String firstHalf(String str) {
+  int len = str.length()/2;
+  return(str.substring(0,len));
+  
+}
+public String makeOutWord(String out, String word) {
+  return(out.substring(0,2)+word+out.substring(2,4));
+}
+public boolean mixStart(String str) {
+  if (str.contains("ix")){
+   if(str.substring(1,3).equals("ix")){
+   return(true);}}
+  else{ 
+   return(false);}
+  return(false);
+  
+}
 
 public String nonStart(String a, String b) {
   int lena = a.length();
@@ -19,6 +35,49 @@ public int diff21(int n) {
    }
   else{
    return(21-n);
+   }
+}
+
+public boolean nearHundred(int n) {
+  if(Math.abs(200-n)<11){
+   return(true);
+   }
+  if(Math.abs(100-n)<11){
+   return(true);
+   }
+  else{
+   return(false);
+   }
+  
+}
+
+
+public int teaParty(int tea, int candy) {
+  if(candy>=5 && tea>=5){
+   if(candy>=2*tea || tea>=2*candy){
+    return(2);
+    }
+   else{
+    return(1);
+    }
+  }
+  else{
+   return(0);
+   }  
+  
+   
+   
+}
+
+public boolean lastDigit(int a, int b, int c) {
+  int lasta = a%10;
+  int lastb = b%10;
+  int lastc = c%10;
+  if (lasta==lastb || lastb==lastc || lasta==lastc) {
+   return true;
+   }
+  else {
+   return false;
    }
 }
 
--- codingbat/firstHalf.java
+++
@@ -1,6 +0,0 @@
-public String firstHalf(String str) {
-  int len = str.length()/2;
-  return(str.substring(0,len));
-  
-}
-
