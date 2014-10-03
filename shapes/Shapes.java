dec5460c13ba9e438be4d06fbf403cfc0f3d11e9
david19kerrigan <david19kerrigan@gmail.com>
2014-10-02 21:44:30 -0400

    d

--- shapes/Shapes.java
+++ shapes/Shapes.java
@@ -25,5 +25,95 @@ public class Shapes{
 		}
 		return result;
 	}
-	 
+	public String tri3(int h){
+		String result="";
+		int r = 1;
+		for(int currentH = 1; currentH<h; currentH++){
+			for(int currentR = 1; currentR<r; currentR++){
+				if((r-currentR)%2!=0){
+					int empties = (r-currentR)/2-.5;
+				}
+				else{
+
+					int empties = (r-currentR)/2;
+				}
+				for(int i = 0; i<empties; i++){
+					result+=(" ");
+				}
+				result+="*";
+				for(int i = 0; i<empties; i++){
+					result+=(" ");
+				}
+			}
+			result+=("\n");
+			r+=1
+		}
+	}
+	public String diamond(int h){
+		String result="";
+		int mid = h/2;
+		int r = 1;
+		for(int currentH = 1; currentH<h; currentH++){
+				
+			if(currentH<mid){
+				for(int currentR =1; currentR<mid; currentR++){
+					if((mid-currentR)%2!=0){
+						int empties = (r-currentR)/2-.5;
+					}
+					else{
+
+						int empties = (r-currentR)/2;
+					}
+					for(int i = 0; i<empties; i++){
+						result+=(" ");
+					}
+					result+="*";
+					for(int i = 0; i<empties; i++){
+						result+=(" ");
+					}
+				}
+			}
+			if(currentH>mid){
+
+				for(int currentR = 1; currentR<r; currentR-=1){
+					if((mid-currentR)%2!=0){
+						int empties = (r-currentR)/2-.5;
+					}
+					else{
+
+						int empties = (r-currentR)/2;
+					}
+					for(int i = 0; i<empties; i++){
+						result+=(" ");
+					}
+					result+="*";
+					for(int i = 0; i<empties; i++){
+						result+=(" ");
+					}
+				}
+			}
+				
+		}
+		return result;
+	}
+	public String tri4(int h){
+		String result = "";
+		int r = h;
+		for(int currentD =0; currentD<d; currentD-=1){
+			for(int currentR = 0; currentR<r; currentR++){
+			        int empties = r-currentR;
+				for(int i = 0; i<empties; i++){
+					result+="";
+				}
+				result+="*";
+			}
+	 		result+="\n";
+			r-=1;
+		}
+		return result;
+	}
+
+
+	
 }	
+	
