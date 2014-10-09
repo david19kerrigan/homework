3f008dc64ad4b4523972b3a6918632c82280ec8d
david19kerrigan <david19kerrigan@gmail.com>
2014-10-08 20:35:42 -0400

    d

---
+++ inheritance/Mage.java
@@ -0,0 +1,16 @@
+public class Mage extends Basechar{
+    private int manna =12345;
+    Private String name;
+
+    public Mage(String name){
+	this.name = name;
+    }
+    public Mage(){
+	this.name="joe";
+    }
+
+    public int getManna(){
+	return manna;
+    }
+    
+}
