bc61478f42bc256925d2a187a5cc999c105b3f03
david.kerrigan <david19kerrigan@gmail.com>
2014-10-22 08:47:51 -0400

    stuayblo

---
+++ loops/Shapes.java
@@ -0,0 +1,29 @@
+
+public class Shapes {
+    public String box(int r, int c) {
+	String s = "";
+
+	while (r>0){
+	    int cc = 0;
+	    while (cc < c){
+		s += "*";
+		cc += 1;
+	    }
+	    s = s + "\n";
+	    r--;
+	}
+
+	return s;
+    }
+    public String tri1(int r){
+	String result = "";
+	int c = 1;
+	for(int cr=0; cr<r; cr++){
+	    for(int cc = 0; cc<c; cc++){
+		result+="*"
+		    }
+		    
+
+	
+    
+}
---
+++ loops/Shapes.java~
@@ -0,0 +1,18 @@
+
+public class Shapes {
+    public String box(int r, int c) {
+	String s = "";
+
+	while (r>0){
+	    int cc = 0;
+	    while (cc < c){
+		s += "*";
+		cc += 1;
+	    }
+	    s = s + "\n";
+	    r--;
+	}
+
+	return s;
+    }
+}
