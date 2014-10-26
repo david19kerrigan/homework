f4b6248aac2c813b7212c61c7bacacf5d4bdc3f7
david19kerrigan <david19kerrigan@gmail.com>
2014-10-26 18:27:55 -0400

    0s

---
+++ 03-strings/Driver.java
@@ -0,0 +1,10 @@
+public class Driver {
+    public static void main(String[] args){
+        String s="david kerrigan";
+       	System.out.println(s.indexOf(' '));
+	int space = s.indexOf(' ');
+        System.out.println(s.substring(0, space));
+	System.out.println(s.substring(space, s.length()));	
+    }
+    
+}
