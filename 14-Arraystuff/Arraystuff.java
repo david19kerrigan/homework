038ee43d73fdf575466a639ad484f0d9aee90c2c
david19kerrigan <david19kerrigan@gmail.com>
2014-10-28 09:26:38 -0400

    xx

--- 14-Arraystuff/Arraystuff.java
+++ 14-Arraystuff/Arraystuff.java
@@ -3,8 +3,10 @@ public class Arraystuff{
     Random rand = new Random();
     private int[] a = new int[100];
     public String toString(){
-	String s = ""+a.length;
-        
+	String s = "";
+        for (int i = 0; i < a.length; i++) {
+	    s = s + a[i]+", ";
+	}
 	return s;
     }
     public Arraystuff(){
@@ -15,7 +17,22 @@ public class Arraystuff{
     }
     public static void main(String[] args){
 	Arraystuff as = new Arraystuff();
-
+	System.out.println(as);
+	System.out.println(as.freq(0));
     }
+
+    
+	
+
+    
+    public int freq(int i){
+	int total = 0;
+	int character=a[i];
+	for(int j = 0; j < a.length; j++){
+	    if(a[j] == character){total++;}
+	}
+	return total;
+    }
+	    
 }
 
