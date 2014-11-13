11b0423b685c15cba561d09f63c1ea6729ae4683
david19kerrigan <david19kerrigan@gmail.com>
2014-11-12 22:46:23 -0500

    array list

--- 15-Sarray/Driver.java
+++ 15-Sarray/Driver.java
@@ -1,8 +1,17 @@
+import java.util.*;
 public class Driver{
-    public static void main(String[] args){
-	Sarray s = new Sarray();
-	System.out.println(s.remove(100));
+	public static void main(String[] args){
+		ArrayList<int> s = new ArrayList<int>();
+		for(int i = 0; i < 21; i++){
+			Random r = new Random();
+			s.add(r.nextInt(9)+1);
+		}
+		int[] newA = new int[10];
+		for(int i = 0; i < s.length; i++){
+			if(newA[s.get(i)]>1){s.remove(i);}
+			else{newA[s.get(i)]++;}
+		}
 
-	   
-    }
+
+	}
 }
