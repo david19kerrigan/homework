d83ba88e7b57517602ee57f3c588c639ed49227f
david19kerrigan <david19kerrigan@gmail.com>
2014-12-03 09:26:11 -0500

    a

---
+++ 20-Sorting/CookieOrder.java
@@ -0,0 +1,15 @@
+public class CookieOrder 
+{ 
+  /** Constructs a new CookieOrder object. */ 
+  public CookieOrder(String variety, int numBoxes) 
+  {  /* implementation not shown */  } 
+  /** @return the variety of cookie being ordered
+   */ 
+  public String getVariety() 
+  {  /* implementation not shown */  } 
+  /** @return the number of boxes being ordered 
+   */ 
+  public int getNumBoxes() 
+  {  /* implementation not shown */  } 
+  // There may be instance variables, constructors, and methods that are not shown.
+} 
